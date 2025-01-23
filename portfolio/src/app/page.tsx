c76b8daa4f49d5b34d721db67aac76662f931cb6
Devvaru <syeraru@gmail.com>
2025-01-22 18:32:08 -0700

    Display project data

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,11 +1,13 @@
 import "../../public/styles/global.css";
 import Navigation from "./navigation";
+import Projects from "./projects";
 
 export default function Home() {
   return (
     <div>
       <Navigation />
       <main>
+        <Projects />
       </main>
       <footer>
       </footer>
